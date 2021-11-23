bfd090192a391bd2766c936685bca50973b4dfb8
Regina28-m <regina-railan2013@yandex.ru>
2021-11-23 20:34:06 +0300

    New

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,24 +1,16 @@
-"use strict";
-
+"use strict"
 const user = new UserForm();
+user.loginFormCallback = function(data) {
+    ApiConnector.login(data, response => {
+        if (!response.success) user.setLoginErrorMessage(response.error);
+        else location.reload();
+    })
+}
 
-user.loginFormCallback = (data) => {
-    ApiConnector.login(data, (response) => {
-        if (response.success) {
-            location.reload();
-        } else {
-            user.setLoginErrorMessage(response.data);
-        }
-    });
-};
-
-user.registerFormCallback = (data) => {
-    ApiConnector.register(data, (response) => {
-        if (response.success) {
-            location.reload();
-        } else {
-            user.setRegisterErrorMessage(response.data);
-        }
-    });
-};
+user.registerFormCallback = function(data) {
+    ApiConnector.register(data, response => {
+        if (!response.success) user.setRegisterErrorMessage(response.error);
+        else location.reload();
+    })
+}
 
